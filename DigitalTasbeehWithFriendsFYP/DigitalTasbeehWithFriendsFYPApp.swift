bd89b30297570f68d49cf4d3a55cf8f2481cdbb3
Huzaifa Ali <huzaifa.ali1@icloud.com>
2025-07-03 17:01:20 +0500

    Initial Commit

---
+++ DigitalTasbeehWithFriendsFYP/DigitalTasbeehWithFriendsFYPApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigitalTasbeehWithFriendsFYPApp.swift
+//  DigitalTasbeehWithFriendsFYP
+//
+//  Created by MacBook 2017 on 03/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct DigitalTasbeehWithFriendsFYPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
